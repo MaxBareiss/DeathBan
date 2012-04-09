e128a1f7317149032586f40d17becdcec5d4bb2f
KingLinkTiger <kinglinktiger@tigerclan.net>
2012-04-09 12:22:29 -0400

    Removed PreLoginListener

--- src/net/tigerclan/KingLinkTiger/DeathBan/DeathBan.java
+++ src/net/tigerclan/KingLinkTiger/DeathBan/DeathBan.java
@@ -22,7 +22,7 @@ import org.bukkit.plugin.java.JavaPlugin;
         public void onEnable() {
         	getServer().getPluginManager().registerEvents(new JoinListener(this.getLogger()), this);
         	getServer().getPluginManager().registerEvents(new DeathListener(this.getLogger()), this);
-        	getServer().getPluginManager().registerEvents(new LoginListener(), this);
+        	//getServer().getPluginManager().registerEvents(new LoginListener(), this);
 
         }
     	/*
